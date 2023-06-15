4850f053169b6824805461d5b20f677e796350ff
Huanyu Luo <lhuany@gmail.com>
2023-06-15 20:30:33 +0800

    Add Non Exempt Encryption Key and  API Key alert.

--- ChatGPT/ChatGPTApp.swift
+++ ChatGPT/ChatGPTApp.swift
@@ -11,12 +11,24 @@ import SwiftUI
 struct ChatGPTApp: App {
     
     let persistenceController = PersistenceController.shared
+    
+    @State var showOpenAIKeyAlert = false
             
     var body: some Scene {
         WindowGroup {
             ContentView()
                 .onAppear() {
                     PromptManager.shared.sync()
+                    if AppConfiguration.shared.key.isEmpty {
+                        showOpenAIKeyAlert = true
+                    }
+                }
+                .alert("Enter OpenAI API Key", isPresented: $showOpenAIKeyAlert) {
+                    TextField("OpenAI API Key", text: AppConfiguration.shared.$key)
+                    Button("Later", role: .cancel) { }
+                    Button("Confirm", role: .none) { }
+                } message: {
+                    Text("You need set OpenAI API Key before start a conversation.")
                 }
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
